26e71e9452e29400f95c35544cde8a20c67735f6
Samantha <someartbysam@gmail.com>
2023-11-22 18:36:11 -0700

    Initial commit

---
+++ src/com/skilldistillers/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,20 @@
+package com.skilldistillers.foodtruck.app;
+
+import com.skilldistillery.foodtruck.entities.FoodTruck;
+
+public class FoodTruckApp {
+
+	private FoodTruck[] fleet = new FoodTruck[5];
+	
+		public static void main(String[] args) {
+			FoodTruckApp app = new FoodTruckApp();
+			app.run();
+			
+		}
+		
+		public void run() {
+			
+		}
+		
+		
+}
