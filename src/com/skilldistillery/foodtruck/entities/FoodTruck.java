3baae4769a76e692e8dc23d457d60978076270fa
Samantha <someartbysam@gmail.com>
2023-11-28 23:45:15 -0700

    Food truck progress

--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,5 +1,29 @@
 package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
+//doesn't have a main
 
+	private String truckName;
+	private String foodType;
+	private int foodRating;
+	
+	public FoodTruck(String truckName, String foodType, int foodRating) {
+		this.truckName = truckName;
+		this.foodType = foodType;
+		this.foodRating = foodRating;
+	}
+	
+	public String getTruckName() {
+		return truckName;
+	}
+	
+	public String getFoodType() {
+		return foodType;
+	}
+	
+	public int getFoodRating() {
+		return foodRating;
+	}
+	
+	
 }
